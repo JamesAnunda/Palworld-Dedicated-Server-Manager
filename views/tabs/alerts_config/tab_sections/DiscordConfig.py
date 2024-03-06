94abe95fbb607f8faa73f28e53a5c2c1a674b5eb
Abaddon16 <logan.gombar@gmail.com>
2024-03-05 18:36:50 -0700

    The GUI is DONE

--- views/tabs/alerts_config/tab_sections/DiscordConfig.py
+++ views/tabs/alerts_config/tab_sections/DiscordConfig.py
@@ -2,7 +2,9 @@ import tkinter as tk
 from tkinter import ttk
 
 from utilities import TkViewElements, Utilities
+from utilities.Constants import SaveSettings
 from utilities.StateInterfaces import IRestorable, ISavable
+from utilities.Utilities import get_or_default
 from views.tabs.alerts_config import AlertsConfig
 
 
@@ -10,12 +12,14 @@ class DiscordConfig(TkViewElements.TkLabelFrame, ISavable, IRestorable):
     def __init__(self, alerts_config: 'AlertsConfig.AlertsConfig', label_text: str = "Discord Config", column: int = 1, row: int = 0, sticky: tk.constants = tk.NSEW):
         super().__init__(alerts_config, label_text, column, row, sticky)
         ttk.Label(self, text="Discord Webhook URL:").grid(column=0, row=0, padx=10, sticky=tk.W)
-        self.discord_webhook_str = tk.StringVar()
-        ttk.Entry(self, textvariable=self.discord_webhook_str, width=35).grid(column=1, row=0)
+        self.discord_webhook = tk.StringVar()
+        ttk.Entry(self, textvariable=self.discord_webhook, width=35).grid(column=1, row=0)
         ttk.Button(self, text="Send Test Message", command=Utilities.send_discord_message).grid(column=1, row=1, columnspan=2, pady=2)
 
     def save(self) -> dict:
-        return {}
+        return {
+                SaveSettings.discord_webhook: get_or_default(self.discord_webhook, SaveSettings.discord_webhook_default)
+        }
 
     def restore(self, restore_data: dict) -> None:
-        pass
+        self.discord_webhook.set(restore_data.get(SaveSettings.discord_webhook, SaveSettings.discord_webhook_default))
